ce23e9e4f5801c5703a47a60f5caebd93846f9c0
Ezran <chas.forelle@gmail.com>
2014-05-08 19:18:27 -0400

    Added ID flag to view

--- main.py
+++ main.py
@@ -9,20 +9,38 @@ def add(args):
 
 def print_list(args):
 	print
-	print "Tag         Note"
-	print "----------  ------------"    
+	id_opt = ""
+	if args.id:
+		id_opt = "oid,"
+		print "ID   Tag         Note"
+		print "---  ----------  ------------"    
+	else:
+		print "Tag         Note"
+		print "----------  ------------"    
 
-	if not args.a:
-		for row in c.execute("select all type,note from main where type=(?)",(args.t,)):
-			print row[0].ljust(11), row[1].ljust(11)
+	if not args.all:
+		for row in c.execute("select all " + id_opt + "type,note from main where type=(?)",(args.t,)):
+			if not args.id:
+				print row[0].ljust(11), row[1].ljust(11)
+			else:
+				print str(row[0]).ljust(4), row[1].ljust(11), row[2].ljust(11)
 	else:	
-		for row in c.execute("select all type,note from main"):
-			print row[0].ljust(11), row[1].ljust(11)
+		for row in c.execute("select all " + id_opt + "type,note from main"):
+			if not args.id:
+				print row[0].ljust(11), row[1].ljust(11)
+			else:
+				print str(row[0]).ljust(4), row[1].ljust(11), row[2].ljust(11)
 	print 
 
 def delete(args):
-	print "Deleterino"
-
+	if args.purge:
+		conf = raw_input("Confirm delete all? [y/n]: ")
+		if conf == y:
+			c.execute("drop table main")
+			print "Confirmed."
+		else:
+			print "Cancelled."
+	
 #initialize the arg parser
 parser = argparse.ArgumentParser(description='A simple note keeping program.')
 subparsers = parser.add_subparsers(help='Sub-command help')
@@ -33,15 +51,16 @@ add_parser.add_argument('-m',help='Note text',required=True)
 add_parser.set_defaults(func=add)
 
 view_parser = subparsers.add_parser('view',help='View notes')
+view_parser.add_argument('-i','--id',help="List IDs of notes",action='store_true',default=None)
 view_group = view_parser.add_mutually_exclusive_group(required=True)
-view_group.add_argument('-a',help='Show all notes',action='store_true')
+view_group.add_argument('--all','-a',help='Show all notes',action='store_true')
 view_group.add_argument('-t',help='Show tagged notes (blank for untagged notes)', const="", nargs='?')
 view_parser.set_defaults(func=print_list)
 
 del_parser = subparsers.add_parser('delete',help='Delete a note')
-del_group = del_parser.add_mutually_exclusive_group()
+del_group = del_parser.add_mutually_exclusive_group(required=True)
 del_group.add_argument('--purge',help='Delete all existing notes',action='store_true')
-del_group.add_argument('--id',help='ID of the note to be deleted')
+del_group.add_argument('-i',help="Note ID to be deleted")
 del_parser.set_defaults(func=delete)
 args = parser.parse_args()
 
